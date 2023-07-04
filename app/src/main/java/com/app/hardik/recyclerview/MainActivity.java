133e5440374614c64c97ffeb354db37bea7c4a8d
hardik <hardik.abril@gmail.com>
2023-07-04 10:26:35 +0530

    simple recyclerview

--- app/src/main/java/com/app/hardik/recyclerview/MainActivity.java
+++ app/src/main/java/com/app/hardik/recyclerview/MainActivity.java
@@ -1,14 +1,45 @@
 package com.app.hardik.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        List<String> data = new ArrayList<>();
+        // Add data to the list
+        data.add("Item 1");
+        data.add("Item 2");
+        data.add("Item 3");
+        data.add("Item 4");
+        data.add("Item 5");
+        data.add("Item 6");
+        data.add("Item 7");
+        data.add("Item 8");
+        data.add("Item 9");
+        data.add("Item 10");
+        data.add("Item 11");
+        data.add("Item 12");
+        data.add("Item 13");
+        data.add("Item 14");
+        data.add("Item 15");
+
+        adapter = new MyAdapter(data);
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/app/hardik/recyclerview/MyAdapter.java
@@ -0,0 +1,42 @@
+package com.app.hardik.recyclerview;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private List<String> data;
+
+    public MyAdapter(List<String> data) {
+        this.data = data;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String item = data.get(position);
+        holder.textView.setText(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView_recyclerView_item);
+        }
+    }
+}
